afbb9c643df38e5a2ef53975e77f315dfc29b6fa
Marvin Sprey <marvin.sprey@web.de>
2023-10-29 23:49:43 +0100

    add small functionality for newsletter form

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,5 +1,22 @@
-export function Newsletter({ heading, text, darkMode }) {
-  return (
+import { useState } from "react"
+
+export function Newsletter({ heading, text }) {
+  const [send, setSend] = useState(false)
+
+  function handleClick() {
+    setSend(true)
+  }
+
+  const thankYou = send ? (
+    <div className="subscribe">
+      <h4>Vielen Dank für dein Interesse!</h4>
+      <span>
+        Wäre an diese Seite ein Backend oder etwas anderes, das serverseitig
+        arbeiten könnte, angeschlossen, könntest du jetzt regelmäßige Newsletter
+        erhalten!
+      </span>
+    </div>
+  ) : (
     <div className="subscribe">
       <h2 className="subscribe__title">{heading}</h2>
       <p className="subscribe__copy">{text}</p>
@@ -9,8 +26,12 @@ export function Newsletter({ heading, text, darkMode }) {
           className="form__email"
           placeholder="Deine Email Adresse"
         />
-        <button className="btn">Abschicken</button>
+        <button onClick={() => handleClick()} className="btn">
+          Abschicken
+        </button>
       </div>
     </div>
   )
+
+  return thankYou
 }
